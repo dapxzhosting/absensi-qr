12ecb5b9073029096fb917443aa9ab3cdf672bf4
Dafa Alfiansyah <alfiansyahdafa10@gmail.com>
2026-06-29 04:44:04 +0700

    Replace Firebase config with placeholders
    Updated Firebase configuration with placeholder values for sensitive information.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBgAGBf9NpL6dkbSDbuR4wwEFEHvGLq_0",
-  authDomain: "absensi-9f088.firebaseapp.com",
+  apiKey: "-----",
+  authDomain: "-----.firebaseapp.com",
   projectId: "absensi-9f088",
-  storageBucket: "absensi-9f088.firebasestorage.app",
-  messagingSenderId: "882675872469",
-  appId: "1:882675872469:web:7eb0759758163658cde85c",
-  measurementId: "G-YY1K3YPJ2P"
+  storageBucket: "-----.firebasestorage.app",
+  messagingSenderId: "-----",
+  appId: "-----",
+  measurementId: "-----"
 }
 
 const app = initializeApp(firebaseConfig)
